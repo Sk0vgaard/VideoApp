46b48072bfc6a8671c503418a30b02d6939d7191
Skovgaard <Sk0vgaard@live.dk>
2017-08-20 15:27:15 +0200

    Add project files.

---
+++ VideoAppUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VideoAppUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
