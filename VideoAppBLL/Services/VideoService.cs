7582359aa8135a7f9110279228fed15f9f313f1c
Skovgaard <Sk0vgaard@live.dk>
2017-08-21 11:42:04 +0200

    Test12345

--- VideoAppBLL/Services/VideoService.cs
+++ VideoAppBLL/Services/VideoService.cs
@@ -21,7 +21,7 @@ namespace VideoAppBLL.Services
             });
             return newVid;
         }
-        //test
+        //test 12345
         public Video Delete(int Id)
         {
             var vid = Get(Id);
