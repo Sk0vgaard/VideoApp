749cbd52cd1b0c68de1421501393f261766603a2
Skovgaard <Sk0vgaard@live.dk>
2017-09-06 17:34:17 +0200

    RenrtalConverter up and running.

---
+++ VideoAppBLL/Converters/RentalConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoAppBLL.BO;
+using VideoAppDAL.Entities;
+
+namespace VideoAppBLL.Converters
+{
+    class RentalConverter
+    {
+        internal Rental Convert(RentalBO rental)
+        {
+            return new Rental()
+            {
+                Id = rental.Id,
+                OrderDate = rental.OrderDate,
+                DeliveryDate = rental.DeliveryDate
+            };
+        }
+
+        internal RentalBO Convert(Rental rental)
+        {
+            return new RentalBO()
+            {
+                Id = rental.Id,
+                OrderDate = rental.OrderDate,
+                DeliveryDate = rental.DeliveryDate
+            };
+        }
+    }
+}
