33cf56707a3157bcf73fb95b2299e28729d76fd6
Skovgaard <Sk0vgaard@live.dk>
2017-09-13 09:37:48 +0200

    All class' and interfaces implemented. Still needs to be coded.

---
+++ VideoAppDAL/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoAppDAL.Entities
+{
+    class User
+    {
+    }
+}
---
+++ VideoAppDAL/Repositories/UserRepositoryEFMemory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoAppDAL.Repositories
+{
+    class UserRepositoryEFMemory
+    {
+    }
+}
